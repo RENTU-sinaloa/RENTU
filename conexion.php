f6c99540ceb55139c8e08dfc66ea1adfced75249
RENTU-sinaloa <54496011+RENTU-sinaloa@users.noreply.github.com>
2019-09-09 09:55:03 -0500

    Update conexion.php

--- conexion.php
+++ conexion.php
@@ -1,9 +1,9 @@
 <?php
 
-    define('DB_SERVER', 'localhost');
-    define('DB_USERNAME', 'root');
-    define('DB_PASSWORD', '');
-    define('DB_NAME', 'login_tuto');
+    define('DB_SERVER', 'postgres: // hmhsvvbbunypud : ae8fe03171e6eef4c0414899553757f04979fb90b6fc81e53bc9490c1b7d3ead @ ec2-23-21-91-183.compute-1.amazonaws.com : 5432 / db9kr5e');
+    define('DB_USERNAME', 'hmhsvvbbunypud');
+    define('DB_PASSWORD', 'ae8fe03171e6eef4c0414899553757f04979fb90b6fc81e53bc9490c1b7d3ead');
+    define('DB_NAME', 'db9kr5egb5oip6');
 
     $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
 
